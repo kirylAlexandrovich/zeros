378c8f014e175c332e3a47a38c556236fbf4b0ff
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2018-09-27 18:00:46 +0300

    count zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count5 = 0;
+  for (let i = 1; i <= number; i++) {
+    let n = i;
+    while (n % 5 === 0) {
+      n = n / 5;
+      count5++;
+    }
+  }
+  return count5;
 }
